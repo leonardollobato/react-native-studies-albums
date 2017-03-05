c6308fa1d3bd41a48f6dbe9d27bb6ba9f2670d38
Leonardo Lobato <leonardolobato@gmail.com>
2017-03-05 16:30:50 -0300

    finishing button component and linking

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailContainerStyle,
     thumbnailStyle,
@@ -34,7 +34,9 @@ const AlbumDetail = ({ album }) => {
         />
       </CardSection>
       <CardSection>
-        <Button onPress={() => console.log(title)} />
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now!
+        </Button>
       </CardSection>
     </Card>
   );
@@ -60,9 +62,9 @@ const styles = {
   },
   imageStyle: {
     height: 300,
-    flex:1,
+    flex: 1,
     width: null
   }
-}
+};
 
 export default AlbumDetail;
